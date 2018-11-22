4726ddfa0d8ac8f278a5bdc8c5af54a0c29f782f
tablada <tabladaraul@gmail.com>
2018-11-22 15:07:35 -0500

    resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import {Grid, Cell} from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
 
 class Resume extends Component {
     render() {
@@ -30,7 +33,54 @@ class Resume extends Component {
                         <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
                         
                     </Cell>
-                    <Cell className="resume-right-col" col={8}>Right Side</Cell>
+                    <Cell className="resume-right-col" col={8}>
+                        <h2>Education</h2>
+                        <Education 
+                            startYear={2013}
+                            endYear={2014}
+                            schoolName="Miami Dade"
+                            schoolDescription="Web Development"
+                        />
+                        <Education
+                            startYear={2017}
+                            endYear={2018}
+                            schoolName="University of Miami"
+                            schoolDescription="Web Development"
+                        />
+                        <hr style={{borderTop: '3px solid #e22947'}} />
+                        <h2>Experience</h2>
+                        <Experience 
+                            startYear={2013}
+                            endYear={2015}
+                            jobName="First Job"
+                            jobDescription="duiehkr"
+                        />
+
+                        <Experience 
+                            startYear={2016}
+                            endYear={2018}
+                            jobName="Second Job"
+                            jobDescription="duiehkr"
+                        />
+                        <hr style={{borderTop: '3px solid #e22947'}} />
+                        <h2>Skills</h2>
+                        <Skills 
+                            skill="javascript"
+                            progress={100}
+                        />
+                        <Skills 
+                            skill="HTML/CSS"
+                            progress={90}
+                        />
+                        <Skills 
+                            skill="NodeJS"
+                            progress={60}
+                        />
+                        <Skills 
+                            skill="React"
+                            progress={67}
+                        />
+                    </Cell>
                 </Grid>
             </div>
         )
