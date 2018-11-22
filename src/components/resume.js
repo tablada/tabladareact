6f778ce05d4e066b776e2e870e73e963e4b9a04d
tablada <tabladaraul@gmail.com>
2018-11-22 14:37:54 -0500

    left side

--- src/components/resume.js
+++ src/components/resume.js
@@ -16,6 +16,19 @@ class Resume extends Component {
                         </div>
                         <h2 style={{paddingTop: '2em'}}>Raul Tablada</h2>
                         <h4 style={{color: 'grey'}}>Programer</h4>
+                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <p>jwrtkburycr</p>
+                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <h5>Address</h5>
+                        <p>12487326 fbdusykerf</p>
+                        <h5>Phone</h5>
+                        <p>346914578</p>
+                        <h5>email</h5>
+                        <p>sekirub@kudf.com</p>
+                        <h5>Web</h5>
+                        <p>website</p>
+                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        
                     </Cell>
                     <Cell className="resume-right-col" col={8}>Right Side</Cell>
                 </Grid>
