92803064cb78ff5f46e632af1e74d2d55ac2694a
tablada <tabladaraul@gmail.com>
2018-11-22 13:09:27 -0500

    contact page w css updates

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,8 +6,21 @@ class Contact extends Component {
         return(
             <div className="contact-body">
                 <Grid className="contact-grid">
-                   <Cell col={6}>half page</Cell>
-                   <Cell col={6}>half page</Cell>
+                   <Cell col={6}>
+                        <h2>Tablada</h2>
+                        <img
+                            src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt="avatar"
+                            style={{height: '250px'}}
+                        />
+                        <p style={{width: '75%', margin: 'auto', paddingTop:'1em'}}>lihwtrubvsruydlsviufcnrlsdtcjvxlihuearhx</p>
+
+
+                   </Cell>
+                   <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+                   </Cell>
                 </Grid>
             </div>
         )
